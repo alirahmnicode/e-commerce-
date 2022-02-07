701c7640e23a1edf245d5d6c6c24815ec951e6a5
alirahmanicode <alirahmanicode@gmail.com>
2022-02-07 18:23:40 +0330

    update models and templates

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product , CategoryForOptions , Option
+from .models import Product , CategoryForOptions , Option , CategoryForProduct
 
 
 admin.site.register(Product)
 admin.site.register(CategoryForOptions)
 admin.site.register(Option)
+admin.site.register(CategoryForProduct)
--- product/models.py
+++ product/models.py
@@ -5,7 +5,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to="product")
-    options = models.ManyToManyField("Option", related_name='option' ,blank=True)
+    options = models.ManyToManyField("Option", related_name="options", blank=True)
+    category = models.ManyToManyField("CategoryForProduct", related_name="categores", blank=True)
     available = models.BooleanField(default=True)
     slug = models.SlugField(null=True)
     create = models.DateTimeField(auto_now_add=True)
@@ -17,13 +18,23 @@ class Product(models.Model):
 
 class Option(models.Model):
     opt = models.CharField(max_length=200)
-    category = models.ForeignKey("CategoryForOptions",on_delete=models.CASCADE , null=True)
-    
+    category = models.ForeignKey(
+        "CategoryForOptions", on_delete=models.CASCADE, null=True
+    )
+
     def __str__(self):
         return self.opt
 
+
 class CategoryForOptions(models.Model):
     name = models.CharField(max_length=200)
-    
+
+    def __str__(self):
+        return self.name
+
+
+class CategoryForProduct(models.Model):
+    name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
