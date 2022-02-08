ed4cb417f73dbc231f14f7e7cb50dd6305d93396
alirahmanicode <alirahmanicode@gmail.com>
2022-02-08 11:16:12 +0330

    start cart app

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local apps
     'product.apps.ProductConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('product.urls'))
+    path('' , include('product.urls')),
+    path('cart/' , include('cart.urls')),
 ]
 
 
