e4f1af7356ce86d221febb97b8c7aa8f06b8cfa0
alirahmanicode <alirahmanicode@gmail.com>
2022-02-13 19:14:47 +0330

    create absolut url

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Product(models.Model):
@@ -15,6 +16,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product:detail', kwargs={'pk': self.pk , 'slug':self.slug})
 
 
 class Option(models.Model):
