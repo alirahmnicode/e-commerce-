78a81e9402b60565da4cb80c57d93c8f763066c1
alirahmanicode <alirahmanicode@gmail.com>
2022-02-17 18:51:38 +0330

    create get products by category

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (Index,product_detail)
+from .views import Index, product_detail, get_products_by_category
 
 app_name = "product"
 
 urlpatterns = [
-    path('' , Index.as_view() , name="index"),
-    path('product/<int:pk>/<str:slug>/' , product_detail , name="detail"),
+    path("", Index.as_view(), name="index"),
+    path("product/<int:pk>/<str:slug>/", product_detail, name="detail"),
+    path("product/<str:category>/", get_products_by_category, name="pr_ct_list"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render, get_object_or_404 , get_list_or_404
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic import ListView
 
-from .models import Product , CategoryForProduct
+from .models import Product, CategoryForProduct
 
 
 class Index(ListView):
     model = Product
     template_name = "index.html"
-    context_object_name = 'products'
+    context_object_name = "products"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['product_categories'] = get_list_or_404(CategoryForProduct)[:10]
+        context["product_categories"] = get_list_or_404(CategoryForProduct)[:10]
         return context
 
 
@@ -21,4 +21,13 @@ def product_detail(request, pk, slug):
         request,
         "product/detail.html",
         {"product": product},
+    )
+
+
+def get_products_by_category(request, category):
+    products = get_list_or_404(Product , category__name=category)
+    return render(
+        request,
+        "product/products_list.html",
+        {"products": products, "category": category},
     )
