a352d816b7459bc7267bc41fa8faa7c78f4982a3
alirahmanicode <alirahmanicode@gmail.com>
2022-03-22 11:16:40 +0430

    fix cart products counter

--- cart/cart.py
+++ cart/cart.py
@@ -88,10 +88,9 @@ class Cart:
         return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
 
     def cart_counter(self):
-        count = 0
+        count = 1
         for item in self.cart.values():
             count+= int(item['quantity'])
-        print(count,'al8'*20)
         self.session['cart_counter'] = int(count)
             
     def save(self):
