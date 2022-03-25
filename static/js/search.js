42f2dfe390d0d37c79b8910eb070214bb129858f
alirahmanicode <alirahmanicode@gmail.com>
2022-03-25 11:55:39 +0430

    complete search box

--- static/js/search.js
+++ static/js/search.js
@@ -6,8 +6,7 @@ searchBox.keyup(function () {
     const query = searchBox[0].value
     var url = `${window.location.origin}/products/search/?q=${query}`
     if (query !== '') {
-        var closeBtn = '<div class="close-popup">&times;</div>'
-        popup.append(closeBtn)
+        $('.close-popup').css('display' , 'block')
         $.ajax({
             type: 'GET',
             url: url,
@@ -18,6 +17,12 @@ searchBox.keyup(function () {
                     popup.css('top', '10%')
                 }
                 response.product.forEach(search_result);
+                response.category.forEach(search_result);
+                console.log(response)
+                if(response.product.length == 0 && response.category.length == 0) {
+                    var alert = '<div class="danger alert">nothing find...</div>'
+                    popup.append(alert)
+                } 
             }
         })
         $('html, body').css({
@@ -25,6 +30,7 @@ searchBox.keyup(function () {
             height: '100%'
         });
     } else {
+        $('.close-popup').css('display' , 'none')
         popup.css('top', '100%')
         popup.empty()
         $('html, body').css({
@@ -36,15 +42,20 @@ searchBox.keyup(function () {
 
 
 function search_result(item, index) {
+    if(item.price == undefined) {
+        var box = `<div>Category of ${item.name}</div>`
+    } else {
+        var box = `<div>
+                        <span>${item.name}</span>
+                        <span>${item.price}</span>
+                    </div>`
+    }
     var htmlCode = `
             <div class="result">
                 <a href="/product/${item.pk}/${item.slug}">
                     <div>
                         <img src="${item.image}" alt="">
-                        <div>
-                            <span>${item.name}</span>
-                            <span>${item.price}</span>
-                        </div>
+                        ${box}
                     </div>
                 </a>
             </div>
