b47578de86133f4882c0263ad908ff248124f470
alirahmanicode <alirahmanicode@gmail.com>
2022-04-23 09:28:47 +0430

    compelete order process

--- order/views.py
+++ order/views.py
@@ -17,24 +17,28 @@ class PaymentView(View):
 
 class OrderProductsView(View):
     def get(self, request):
-        try:
-            profile = Profile.objects.get(user=request.user)
-            cart = Cart(request)
-            # create order instance
-            order = Order()
-            # set values
-            order.price_paid = cart.get_total_price()
-            order.user_info = profile
-            order.trackingcode = tracking_code()
-            order.payment = True
-            order.save()
-            # set products
-            for product_id in cart.cart:
-                product = Product.objects.get(pk=product_id)
-                order.products.add(product.pk)
-            cart.clear()
-            messages.success(request, "Your purchase has been successfully registered , you can see factors in dashboard")
-            return redirect(request.META.get("HTTP_REFERER"))
-        except:
-            messages.error(request, "Your purchase has been not registered , try again")
+        if request.user.is_authenticated:
+            try:
+                profile = Profile.objects.get(user=request.user)
+                cart = Cart(request)
+                # create order instance
+                order = Order()
+                # set values
+                order.price_paid = cart.get_total_price()
+                order.user_info = profile
+                order.trackingcode = tracking_code()
+                order.payment = True
+                order.save()
+                # set products
+                for product_id in cart.cart:
+                    product = Product.objects.get(pk=product_id)
+                    order.products.add(product.pk)
+                cart.clear()
+                messages.success(request, "Your purchase has been successfully registered , you can see factors in dashboard")
+                return redirect(request.META.get("HTTP_REFERER"))
+            except:
+                messages.error(request, "Your purchase has been not registered.if you have not profile , create in dashboard")
+                return redirect(request.META.get("HTTP_REFERER"))
+        else:
+            messages.error(request, "Your are not logged in")
             return redirect(request.META.get("HTTP_REFERER"))
